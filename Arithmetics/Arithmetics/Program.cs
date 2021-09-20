f2bbf8c8aa6a34603ee5b0f3c3aace729de1c1a2
Belov-AI <Belov.Alexander@urfu.ru>
2021-09-20 16:06:47 +0500

    Начат проект Arithmettics

---
+++ Arithmetics/Arithmetics/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Arithmetics
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = 2;
+            const double g = 9.8;
+
+            Console.WriteLine(n);
+            Console.WriteLine(g);
+            Console.WriteLine(n++ + ++n);
+            Console.WriteLine(n);
+
+            Console.ReadKey();
+        }
+    }
+}

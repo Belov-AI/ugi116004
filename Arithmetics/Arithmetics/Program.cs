f896142b655f1d5c30c7833b469f116e7a655ab7
Belov-AI <Belov.Alexander@urfu.ru>
2021-09-27 14:30:46 +0500

    Arithmetics: Конверсия типов

--- Arithmetics/Arithmetics/Program.cs
+++ Arithmetics/Arithmetics/Program.cs
@@ -14,7 +14,59 @@ namespace Arithmetics
             Console.WriteLine(n++ + ++n);
             Console.WriteLine(n);
 
+            int x = 3, y = 4, z = 5;
+            Console.WriteLine(x + y * z);
+
+            x = y += 2;
+            Console.WriteLine(x);
+
+            double angle = Math.PI/6;
+            double sinusX = Math.Sin(angle);
+            Console.WriteLine(sinusX);
+
+            //int k;
+            //k = 1;
+
+            var k = "1";
+            Console.WriteLine(k);
+
+            angle = x;
+            Console.WriteLine(angle);
+
+            x = (int)Math.Pow(2, 3);
+            Console.WriteLine(x);
+
+            x = (int)Math.Sqrt(3);
+            Console.WriteLine(x);
+
+            x = (int)Math.Round(Math.Sqrt(3));
+            Console.WriteLine(x);
+
+            Console.WriteLine((int)(-1.5));
+            Console.WriteLine(Math.Floor(-1.5));
+
+            int integerNumber;
+            long longNumber;
+
+            checked
+            {
+                longNumber = 100;
+                integerNumber = (int)longNumber;
+                Console.WriteLine(integerNumber);
+
+                //longNumber = 3000000000L;
+                //integerNumber = (int)longNumber;
+                //Console.WriteLine(integerNumber);
+            }
+
+            Console.WriteLine(Square(5));
+            
             Console.ReadKey();
+        }
+
+        static int Square(int number)
+        {
+            return number * number;
         }
     }
 }

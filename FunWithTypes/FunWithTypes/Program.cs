117af49349d1b397317e797312a1ea21c503f437
Belov-AI <Belov.Alexander@urfu.ru>
2021-09-27 16:07:44 +0500

    Решено

--- FunWithTypes/FunWithTypes/Program.cs
+++ FunWithTypes/FunWithTypes/Program.cs
@@ -7,20 +7,20 @@ namespace FunWithTypes
         static void Main()
         {
             double pi = Math.PI;
-            int oneThousand = 1000L; 
-            float piF = pi;
-            float oneThousandPi = pi * oneThousand;
-            int rounded1000Pi = oneThousandPi; //тут должно правильно округляться
-            int integerPart = oneThousandPi; //а тут нужно вычислить целую часть
+            int oneThousand = (int)1000L; 
+            float piF = (float)pi;
+            float oneThousandPi = (float)(pi * oneThousand);
+            int rounded1000Pi = (int)Math.Round(oneThousandPi); //тут должно правильно округляться
+            int integerPart = (int)Math.Floor(oneThousandPi); //а тут нужно вычислить целую часть
             Console.WriteLine(rounded1000Pi);
-            Console.WriteLine(integrePart);       
-            
-            var a = 10;
+            Console.WriteLine(integerPart);
+
+            double a = 10;
             a += 0.5; //надо исправить, чтоб работало
             Console.WriteLine(a);
 
             var b = "15";
-            var c = a + b; //тут что-то не так! надо поправить
+            var c = a + double.Parse(b); //тут что-то не так! надо поправить
             Console.WriteLine(c);
             Console.ReadKey();
         }

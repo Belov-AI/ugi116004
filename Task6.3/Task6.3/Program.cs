f8b808aba08321c1ab28f3357f95f04a4f7677b1
Belov-AI <Belov.Alexander@urfu.ru>
2021-11-15 13:40:56 +0500

    Задание 6.3 завершено

--- Task6.3/Task6.3/Program.cs
+++ Task6.3/Task6.3/Program.cs
@@ -14,22 +14,39 @@ namespace Task6._3
             Console.WriteLine("Введите позицию белого ферзя");
             var whiteQueenPosition = Console.ReadLine();
 
-            //int queenX, queenY;
-            //GetCoordinates(whiteQueenPosition, out queenY, out queenX);
-            //Console.WriteLine($"{queenX} {queenY}");
-
             Console.WriteLine("Введите позицию черной пешки");
             var blackPawnPosition = Console.ReadLine();
 
-            Console.WriteLine(IsPawnStrikesQueen(whiteQueenPosition, blackPawnPosition));
+            if (!IsFiguresPositionsCorrect(whiteQueenPosition, blackPawnPosition))
+            {
+                Console.WriteLine("Позиции фигур не соответствуют условиям задачи");
+                Console.ReadKey();
+                return;
+            }
+
+            Console.WriteLine("Введите ход белого ферзя");
+            var move = Console.ReadLine();
+
+            if (IsQueenCanMakeSafeMove(whiteQueenPosition, blackPawnPosition, move))
+                Console.WriteLine($"Ферзь {whiteQueenPosition} может безопасно пойти на клетку {move}");
+            else
+                Console.WriteLine($"Ферзь {whiteQueenPosition} не может пойти на клетку {move}");
 
             Console.ReadKey();
         }
 
-        static void GetCoordinates(string position, out int row, out int column)
+        static bool IsFiguresPositionsCorrect(string queenPosition, string pawnPosition)
         {
-            column = (int)position[1] - 0x31;
-            row = (int)position[0] - 0x61;
+            return !IsEqualPositions(queenPosition, pawnPosition) &&
+                IsPawnPositionCorrect(pawnPosition) &&
+                !IsQueenStrikesPawn(queenPosition, pawnPosition) &&
+                !IsPawnStrikesQueen(queenPosition, pawnPosition);
+        }
+
+        static bool IsQueenCanMakeSafeMove(string queenPosition, string pawnPosition, string move)
+        {
+            return IsQueenCanMove(queenPosition, move) &&
+                !IsPawnStrikesQueen(move, pawnPosition);
         }
 
         static bool IsEqualPositions(string position1, string position2)
@@ -45,6 +62,34 @@ namespace Task6._3
             GetCoordinates(pawnPosition, out py, out px);
 
             return py - qy == 1 && Math.Abs(px - qx) == 1;
+        }
+
+        static bool IsQueenStrikesPawn(string queenPosition, string pawnPosition)
+        {
+            return IsQueenCanMove(queenPosition, pawnPosition);
+        }
+
+        static bool IsPawnPositionCorrect(string pawnPosition)
+        {
+            int px, py;
+            GetCoordinates(pawnPosition, out py, out px);
+
+            return py > 0 && py < 7;
+        }
+
+        static bool IsQueenCanMove(string queenPosition, string move)
+        {
+            int qx, qy, mx, my;
+            GetCoordinates(queenPosition, out qy, out qx);
+            GetCoordinates(move, out my, out mx);
+
+            return qy == my || qx == mx || Math.Abs(qy - my) == Math.Abs(qx - mx); 
+        }
+
+        static void GetCoordinates(string position, out int row, out int column)
+        {
+            row = (int)position[1] - 0x31;
+            column = (int)position[0] - 0x61;
         }
     }
 }

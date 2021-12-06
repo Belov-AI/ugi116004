4b171ff6d1b31bb7d1b374c47858cc3cb1ffa3e7
Belov-AI <Belov.Alexander@urfu.ru>
2021-12-06 13:56:55 +0500

    Классы и объекты

---
+++ ClasssesAndObjects/ClasssesAndObjects/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasssesAndObjects
+{
+    public class Person
+    {
+        public static string Species = "Homo Sapience";
+
+        public string Name;
+        public string Surname;
+        public int Age;
+
+        static void PrintSpecies()
+        {
+            Console.WriteLine(Species);
+        }
+    }
+}
---
+++ ClasssesAndObjects/ClasssesAndObjects/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasssesAndObjects
+{
+    class Program
+    {
+        static int number = 1;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(number);
+            Console.WriteLine(Person.Species);
+
+            var peter = new Person();
+            peter.Name = "Пётр";
+            peter.Surname = "Иванов";
+            peter.Age = 17;
+            PrintPerson(peter);
+
+            var ann = new Person() { Name = "Анна", Surname = "Сергеева", Age = 18 };
+            PrintPerson(ann);
+
+            Console.ReadKey();
+        }
+
+        static void PrintPerson(Person person)
+        {
+            Console.WriteLine($"{Person.Species}: {person.Name} {person.Surname}, возраст: {person.Age}");
+        }
+    }
+}

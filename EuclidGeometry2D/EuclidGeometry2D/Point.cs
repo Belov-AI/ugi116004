1960df9e6785fb125b585c952f73878a1d5896a3
Belov-AI <Belov.Alexander@urfu.ru>
2022-02-14 19:23:26 +0500

    Начало проекта EuclidGeometry2D: классы Point и Segment

---
+++ EuclidGeometry2D/EuclidGeometry2D/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EuclidGeometry2D
+{
+    public class Point
+    {
+        public double X;
+        public double Y;
+
+        public Point(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
---
+++ EuclidGeometry2D/GeometryConsoleApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EuclidGeometry2D;
+
+namespace GeometryConsoleApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            var point = new Point(1, 2);
+            PrintPoint(point);
+
+            var segment = new Segment(new Point(1, 1), new Point(3, 4));
+            PrintSegment(segment);
+
+            segment.A.X = -1;
+            PrintSegment(segment);
+
+            Console.ReadKey();
+        }
+
+        static void PrintPoint(Point p)
+        {
+            Console.WriteLine($"точка ({p.X}; {p.Y})");
+        }
+
+        static void PrintSegment(Segment s)
+        {
+            Console.WriteLine($"отрезок [({s.A.X}; {s.A.Y}), ({s.B.X}; {s.B.Y})] длиной {s.Length:F3}");
+        }
+    }
+}

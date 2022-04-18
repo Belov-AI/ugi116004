2ba31320e5e4f110ae8522a6b19dcef84807a05d
Belov-AI <Belov.Alexander@urfu.ru>
2022-04-18 19:15:47 +0500

    MDI

---
+++ Forms/Forms/ContainerForm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Forms
+{
+    public partial class ContainerForm : Form
+    {
+        int documentNumber = 0;
+
+        public ContainerForm()
+        {
+            InitializeComponent();
+        }
+
+        private void newDocumentToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            var document = new DocumentForm();
+            document.MdiParent = this;
+            documentNumber++;
+            document.Text = "Документ " + documentNumber;
+            document.Show();
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}

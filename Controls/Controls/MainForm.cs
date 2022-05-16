559b1e4303eacd61d9556dc27bd896975c69d935
Belov-AI <Belov.Alexander@urfu.ru>
2022-05-16 19:19:43 +0500

    Элементы управления - надписи, поле ввода, кнопки, флажки, радиокнопки

--- Controls/Controls/MainForm.cs
+++ Controls/Controls/MainForm.cs
@@ -15,6 +15,63 @@ namespace Controls
         public MainForm()
         {
             InitializeComponent();
+
+            //var button = new Button();
+            //button.Location = new Point(100, 200);
+            //button.Text = "Click me";
+            //Controls.Add(button);
+        }
+
+        private void inputTextBox_TextChanged(object sender, EventArgs e)
+        {
+            if (inputTextBox.Text.Length > 0)
+            {
+                inputButton.Visible = true;
+                inputLabel.Visible = false;
+            }
+
+            else
+            {
+                inputButton.Visible = false;
+                inputLabel.Visible = true;
+            }
+                
+        }
+
+        private void inputButton_Click(object sender, EventArgs e)
+        {
+            captionLabel.Text = inputTextBox.Text;
+            inputTextBox.Text = "";
+        }
+
+        private void ChangeFontStyle(object sender, EventArgs e)
+        {
+            var fontStyle = FontStyle.Regular;
+
+            if (boldCheckBox.Checked)
+                fontStyle |= FontStyle.Bold;
+
+            if (italicCheckBox.Checked)
+                fontStyle |= FontStyle.Italic;
+
+            if (undelineCheckBox.Checked)
+                fontStyle |= FontStyle.Underline;
+
+            captionLabel.Font = new Font(
+                captionLabel.Font.FontFamily,
+                captionLabel.Font.Size,
+                fontStyle);
+        }
+
+        private void ChangeFontSize(object sender, EventArgs e)
+        {
+            var radiobutton = sender as RadioButton;
+            int size = int.Parse(radiobutton.Text.Split()[0]);
+
+            captionLabel.Font = new Font(
+                captionLabel.Font.FontFamily,
+                size,
+                captionLabel.Font.Style);
         }
     }
 }
